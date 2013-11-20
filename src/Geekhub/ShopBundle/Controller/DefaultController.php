4357a7a79142af1db2a7479627946f93cfff6cec
Viktor Musienko <treasury23@gmail.com>
2013-11-21 00:28:31 +0200

    add controller and views

--- src/Geekhub/ShopBundle/Controller/DefaultController.php
+++ src/Geekhub/ShopBundle/Controller/DefaultController.php
@@ -3,11 +3,24 @@
 namespace Geekhub\ShopBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Doctrine\ORM\EntityManager;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('GeekhubShopBundle:Default:index.html.twig');
+    }
+
+    public function productAction()
+    {
+        $product = $this->getDoctrine()->getRepository('GeekhubShopBundle:Product');
+
+        return $this->render('GeekhubShopBundle:Default:product.html.twig', array('products' => $product->findAll()));
+    }
+
+    public function infoAction($id)
+    {
+
     }
 }
